a48901b6378a2c091cd339bc1c6b519caa794021
AllSungho <nanumeran@gmail.com>
2025-08-19 11:13:12 +0900

    Update field in PostUpdateRequest Dto

--- src/main/java/hello/newsfeed/post/dto/PostUpdateRequest.java
+++ src/main/java/hello/newsfeed/post/dto/PostUpdateRequest.java
@@ -1,4 +1,11 @@
 package hello.newsfeed.post.dto;
 
+import lombok.Getter;
+
+@Getter
 public class PostUpdateRequest {
+
+    private String title;
+    private String content;
+    private String postImage;
 }
