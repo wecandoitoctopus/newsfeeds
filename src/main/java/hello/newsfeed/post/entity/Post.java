a328faa12d021648bee4aa0ea5525f98bd3a9b84
AllSungho <nanumeran@gmail.com>
2025-08-19 10:59:12 +0900

    Update field and constructor in Post Entity

--- src/main/java/hello/newsfeed/post/entity/Post.java
+++ src/main/java/hello/newsfeed/post/entity/Post.java
@@ -1,17 +1,26 @@
 package hello.newsfeed.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor
-public class Post {
+public class Post extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    // 일단 이미지를 스트링으로 대체하겠습니다.
+    private String postImage;
+
+    public Post(String title, String content, String postImage) {
+        this.title = title;
+        this.content = content;
+        this.postImage = postImage;
+    }
 }
