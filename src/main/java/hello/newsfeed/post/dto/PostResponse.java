99927a9875529eb6f72b7ebe3825467924c563fe
AllSungho <nanumeran@gmail.com>
2025-08-19 11:07:51 +0900

    Update field and constructor in PostResponse Dto

--- src/main/java/hello/newsfeed/post/dto/PostResponse.java
+++ src/main/java/hello/newsfeed/post/dto/PostResponse.java
@@ -1,4 +1,23 @@
 package hello.newsfeed.post.dto;
 
+import hello.newsfeed.post.entity.Post;
+import lombok.Getter;
+
+@Getter
 public class PostResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String postImage;
+
+    private PostResponse(Long id, String title, String content, String postImage) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.postImage = postImage;
+    }
+    public PostResponse createPostResponse(Post post) {
+        return new PostResponse(post.getId(), post.getTitle(), post.getContent(), post.getPostImage());
+    }
 }
