ad6b81f30df6f3d0c905e9a64617e6e603ec4d82
leetwin1115 <jjeepp1115@gmail.com>
2025-08-19 20:26:02 +0900

    like create

--- src/main/java/hello/newsfeed/like/entity/Like.java
+++ src/main/java/hello/newsfeed/like/entity/Like.java
@@ -1,17 +1,54 @@
 package hello.newsfeed.like.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import hello.newsfeed.post.entity.Post;
+import hello.newsfeed.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.security.Timestamp;
+
+@Builder
 @Getter
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "likes",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "likes",
+                        columnNames={"post_id", "user_id"}
+                )
+        }
+)
 public class Like {
+
+//    @Transient // 칼럼이 만들어지지 않는다.
+//    private Long likeCount;
+//
+//    @Transient
+//    private boolean likeState;
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+//    @CreationTimestamp
+//    private Timestamp createAt;
+
+    public Like(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
 }
