dcbc13c334ebe6691dda106a3034533610e330b9
Xellos216 <xellos216@gmail.com>
2025-08-20 20:58:02 +0900

    refactor: user module, add validation & global exception handler

--- src/main/java/hello/newsfeed/user/dto/ChangePasswordPatchRequest.java
+++ src/main/java/hello/newsfeed/user/dto/ChangePasswordPatchRequest.java
@@ -2,4 +2,14 @@ package hello.newsfeed.user.dto;
 
 import jakarta.validation.constraints.*;
 
-public record ChangePasswordPatchRequest(@NotBlank @Size(min = 6, max = 60) String newPassword) {}
+public record ChangePasswordPatchRequest(
+        @NotBlank(message = "New password is required.")
+        @Size(min = 6, max = 60, message = "Current password must be at least 6 characters")
+        String currentPassword,
+
+        @NotBlank(message = "New password is required.")
+        @Size(min = 6, max = 60, message = "New password must be at least 6 characters")
+        String newPassword) {
+}
+
+// -> 필수 구현 비밀번호 예외 처리 2번
